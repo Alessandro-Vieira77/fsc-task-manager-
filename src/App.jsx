c0101fc4856fe5f670960fd201a13bddc34f4b0b
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-08-17 17:04:36 -0300

    chore: add taiwindcss setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>Hello world!!</h1>
+        <h1 className=" bg-red-500 text-blue-600 font-bold">Hello world!!</h1>
       </div>
     </>
   );

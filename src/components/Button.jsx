d651d6b703a0bbf05ebdf85ff0064eb0e04df3ab
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-08-20 10:55:22 -0300

    feat:  add size and secundary variant to button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,10 @@
-const Button = ({ children, variant = "primary", ...props }) => {
+const Button = ({
+  children,
+  variant = "primary",
+  size = "small",
+  className,
+  ...props
+}) => {
   function getVariantClass() {
     if (variant === "primary") {
       return "bg-[#00ADB5] text-white";
@@ -7,11 +13,25 @@ const Button = ({ children, variant = "primary", ...props }) => {
     if (variant === "Ghost") {
       return "text-[#818181]";
     }
+
+    if (variant === "secundary") {
+      return "text-[#35383E] bg-[#EEEEEE]";
+    }
+  }
+
+  function getSizeClass() {
+    if (size === "small") {
+      return "px-3 py-1 text-xs font-semibold";
+    }
+
+    if (size === "large") {
+      return "px-3 py-4 text-sm  ";
+    }
   }
 
   return (
     <button
-      className={`flex h-6 items-center gap-1 rounded-md border-[#00ADB5] px-3 py-1 text-xs font-semibold transition hover:bg-opacity-75 ${getVariantClass()}`}
+      className={`flex h-6 items-center justify-center gap-1 rounded-md border-[#00ADB5] font-semibold transition hover:bg-opacity-75 ${getVariantClass()} ${getSizeClass()} ${className}`}
       {...props}>
       {children}
     </button>

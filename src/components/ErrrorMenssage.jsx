c3548b390146f8363697830ba16539301f6804d1
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-08-24 00:41:30 -0300

    refactor: add prop-types to ErrorMenssage component

--- src/components/ErrrorMenssage.jsx
+++ src/components/ErrrorMenssage.jsx
@@ -1,9 +1,15 @@
+import PropTypes from "prop-types";
+
 const ErrorMenssage = ({ children }) => {
   return (
     <span className="w-full pt-1 text-left text-xs text-brand-danger">
       {children}
     </span>
   );
+};
+
+ErrorMenssage.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default ErrorMenssage;

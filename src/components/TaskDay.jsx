12c716c0b5213bdeefa51361406dc67d5a1c9681
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-08-24 00:51:20 -0300

    refactor: add prop-types to TaskDay component

--- src/components/TaskDay.jsx
+++ src/components/TaskDay.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 const TaskDay = ({ children, title }) => {
   return (
     <div className="flex w-full items-center gap-2 border-b-2 border-brand-border p-3 text-sm font-semibold text-brand-text-gray">
@@ -5,6 +7,11 @@ const TaskDay = ({ children, title }) => {
       <p>{title}</p>
     </div>
   );
+};
+
+TaskDay.propTypes = {
+  children: PropTypes.node.isRequired,
+  title: PropTypes.string.isRequired,
 };
 
 export default TaskDay;

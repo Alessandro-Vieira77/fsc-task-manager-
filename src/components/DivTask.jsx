c80f1ec3914e64da0b3f2ddd088e9cbaa58dcbbd
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-08-24 00:40:03 -0300

    refactor: add prop-types to DivTask component

--- src/components/DivTask.jsx
+++ src/components/DivTask.jsx
@@ -1,5 +1,10 @@
+import PropTypes from "prop-types";
 const DivTask = ({ children }) => {
   return <div className="w-full space-y-3">{children}</div>;
+};
+
+DivTask.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default DivTask;
